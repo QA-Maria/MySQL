1e039c73217b20420186d43e39963fc65ef0f826
QA-Maria <103017690+QA-Maria@users.noreply.github.com>
2022-07-18 11:34:27 +0400

    Update collection_sql.sql

--- collection_sql.sql
+++ collection_sql.sql
@@ -1,1 +1,1 @@
-SELECT * FROM airplane.airplane_pilot, airplane.pilot, airplane.airplane;
+SELECT * FROM airplane.airplane_pilot, airplane.pilot, airplane.airplane; # Вывод из БД Airplane все таблицы
