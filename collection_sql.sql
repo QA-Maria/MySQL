3cb0f1ce502fc0b12d5909620d8f16202c94858c
QA-Maria <103017690+QA-Maria@users.noreply.github.com>
2022-07-18 12:40:13 +0400

    Update collection_sql.sql

--- collection_sql.sql
+++ collection_sql.sql
@@ -1,1 +1,7 @@
 SELECT * FROM airplane.airplane_pilot, airplane.pilot, airplane.airplane; # Вывод из БД Airplane все таблицы
+
+SELECT * FROM airplane.pilot ORDER BY first_name DESC; # 
+
+SELECT * FROM airplane.airplane_pilot WHERE pilot_id = 1 order by id DESC;
+
+SELECT COUNT(*) as count FROM airplane_pilot WHERE airplane_id=1;
